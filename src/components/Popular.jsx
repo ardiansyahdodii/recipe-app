09a52555756f70a6b25f054665afe2ecdc4e3efd
ardiansyahdodii <dodyn890@gmail.com>
2024-01-31 07:15:26 +0700

    tombol slide

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,5 +1,9 @@
+import { Navigation, Pagination, A11y } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import 'swiper/swiper-bundle.css';
+// import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import { useEffect, useState } from 'react';
 import { getPopularRecipe } from '../utils/api';
 import CardPopular from './CardPopular';
@@ -18,13 +22,16 @@ const Popular = () => {
         <div className="mt-10 p-2">
             <h1 className="text-2xl font-bold">Popular Recipe</h1>
             <Swiper
+                modules={[Navigation, Pagination, A11y]}
                 spaceBetween={25}
                 slidesPerView={3.5}
-                className='mt-5'
+                navigation={true}
+                pagination={{ clickable: true }}
+                className='mySwiper mt-5'
             >
                 {popularRecipe.map((recipe) => (
-                    <SwiperSlide key={recipe.id}>
-                        <CardPopular id={recipe.id} title={recipe.title} image={recipe.image} ready={recipe.readyInMinutes} score={recipe.spoonacularScore} type={recipe.dishTypes[0]} like={recipe.aggregateLikes}/>
+                    <SwiperSlide key={recipe.id}>  
+                        <CardPopular id={recipe.id} title={recipe.title} image={recipe.image} ready={recipe.readyInMinutes} score={recipe.spoonacularScore} type={recipe.dishTypes[0]} like={recipe.aggregateLikes} />
                     </SwiperSlide>
                 ))}
             </Swiper>
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -15,14 +15,14 @@ const SearchPage = () => {
 
     }, [searchType])
 
-    // console.log(recipes)
+    console.log(recipes)
 
     return (
         <div>
             <h3 className="text-xl font-bold mt-5">Kata kunci : {searchType}</h3>
             <div className="grid grid-cols-5 gap-5 mt-5">
                 {recipes.map((recipe) => (
-                    <CardRecipe key={recipe.id} title={recipe.title} image={recipe.image} />
+                    <CardRecipe key={recipe.id} title={recipe.title} image={recipe.image} id={recipe.id} />
                 ))}
             </div>
         </div>
